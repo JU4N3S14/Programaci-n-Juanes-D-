cb677ef3f3c47a8022d0ba05ad6ce9be9ca55bcc
JU4N3S14 <168590780+JU4N3S14@users.noreply.github.com>
2025-05-11 02:14:57 +0000

    prueba

---
+++ build/CMakeFiles/trabajofinal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trabajofinal.dir/main.cpp.o"
+  "trabajofinal"
+  "trabajofinal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trabajofinal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
